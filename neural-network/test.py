ae709301d627ea37737b53441ed3759f3e68be04
Jordon Dornbos <jordon.dornbos@gmail.com>
2014-12-15 16:59:52 -0500

    Final code

--- neural-network/test.py
+++ neural-network/test.py
@@ -13,12 +13,12 @@ LOG_FILENAME = 'neural-network.log'
 logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
 
 
-def build_map(file, break_word=''):
+def build_map(file, break_word=None):
     map = {}
 
     # put data in maps
     for line in file:
-        if break_word in line and break_word is not '':
+        if break_word is not None and break_word in line:
             break
 
         line = re.sub('[\n]', '', line)  # delete newline characters
@@ -52,9 +52,6 @@ def get_normalized_data(filename):
 
 
 def get_data(filename, time_map, distance_map, carrier_map, airport_map):
-    # array of Examples
-    data = []
-
     # open the data file
     file = open(filename)
 
@@ -62,22 +59,30 @@ def get_data(filename, time_map, distance_map, carrier_map, airport_map):
     next(file)
 
     # convert data file into Example objects
+    data = []
     for line in file:
         line = re.sub('[\n]', '', line)  # delete newline characters
-
-        # split the data up
-        values = line.split(',')
+        values = line.split(',')  # split the data up
 
         # only consider flights that haven't been canceled
-        if float(values[21]) is not 1.0:
+        if float(values[21]) != 1.0:
             x = []
             y = []
 
             try:
+                # get normalized inputs
                 dept_time = float(time_map[values[5][:-2]][2])
                 carrier = float(carrier_map[values[8]][2])
-                delay = float(values[15])
                 airport = float(airport_map[values[16]][2])
+
+                # get delay and apply threshold
+                delay = float(values[15])
+                if delay > 15:
+                    delay = 1.0
+                else:
+                    delay = 0.0
+
+                # get distance and find normalized value from range
                 distance = float(values[18])
                 if distance < 300:
                     distance = float(distance_map['0'][2])
@@ -88,14 +93,12 @@ def get_data(filename, time_map, distance_map, carrier_map, airport_map):
                 else:
                     distance = float(distance_map['3'][2])
 
+                # put the values in the arrays
                 x.append(dept_time)
                 x.append(carrier)
                 x.append(airport)
                 x.append(distance)
-                if delay > 15:
-                    y.append(1.0)
-                else:
-                    y.append(0.0)
+                y.append(delay)
 
             except ValueError:
                 logging.debug('Could not add {0} because of an invalid value'.format(values))
@@ -104,9 +107,9 @@ def get_data(filename, time_map, distance_map, carrier_map, airport_map):
                 logging.debug('Could not find key for value in {0}'.format(values))
                 continue
 
-            # add the Example object
-            logging.debug('Adding data point {0}, {1}]'.format(x, y))
-            data.append(example.Example(x,y))
+            # add the data point
+            logging.debug('Adding data point [{0}, {1}]'.format(x, y))
+            data.append(example.Example(x, y))
 
         else:
             logging.debug('Found a canceled flight')
@@ -114,28 +117,28 @@ def get_data(filename, time_map, distance_map, carrier_map, airport_map):
     return data
 
 
-def filter(training_data):
-    filtered = []
+def order(training_data):
+    ordered = []
 
     pos = 0
     neg = 0
-    data = len(training_data)
+    data_len = len(training_data)
     pick_pos = True
-    while pos < data and neg < data:
+    while pos < data_len and neg < data_len:
         if pick_pos:
             if training_data[pos].y[0] == 1.0:
                 logging.debug('Adding positive set')
-                filtered.append(training_data[pos])
+                ordered.append(training_data[pos])
                 pick_pos = False
             pos += 1
         else:
             if training_data[neg].y[0] == 0.0:
                 logging.debug('Adding negative set')
-                filtered.append(training_data[neg])
+                ordered.append(training_data[neg])
                 pick_pos = True
             neg += 1
 
-    return filtered
+    return ordered
 
 
 def train(examples, alpha, iteration_max, num_hidden_layers, num_nodes_per_hidden_layer, weights=None, verbose=False):
@@ -156,21 +159,26 @@ def train(examples, alpha, iteration_max, num_hidden_layers, num_nodes_per_hidde
     return hypothesis_network
 
 
-def test(network, verification_data):
+def test(network, verification_data, verbose=False):
     logging.info('Testing accuracy...')
     num_delay_correct = 0
     num_delay_incorrect = 0
     num_on_time_correct = 0
     num_on_time_incorrect = 0
     for test in verification_data:
+        # get guess and actual value
         output = network.guess(test.x)[0]
         actual = test.y[0]
-        # logging.info('Output: {0:.3f} Actual: {1}'.format(output, actual))
+        if verbose:
+            logging.info('Output: {0:.3f} Actual: {1}'.format(output, actual))
+
+        # apply threshold
         if output > 0.5:
             output = 1.0
         else:
             output = 0.0
 
+        # add to statistics
         if output == actual:
             if actual == 1.0:
                 num_delay_correct += 1
@@ -189,12 +197,15 @@ def test(network, verification_data):
 
     average_error = float(num_delay_incorrect + num_on_time_incorrect) / \
                     (num_delay_correct + num_delay_incorrect + num_on_time_correct + num_on_time_incorrect)
-    logging.info('Average accuracy was: {0:.3f}'.format(1.0 - average_error))
+    average_accuracy = 1.0 - average_error
+    logging.info('Average accuracy was: {0:.3f}'.format(average_accuracy))
     logging.info('Average error was: {0:.3f}'.format(average_error))
+
+    return average_accuracy
 
 
 def main():
-    # get training data and verification data
+    # get training and verification data
     logging.info('Loading data...')
     normalized_data = get_normalized_data('../data/normalized/2004_output.txt')
     training_data = get_data('../data/flight/2004_subset.csv', normalized_data[0], normalized_data[1],
@@ -203,17 +214,32 @@ def main():
     verification_data = get_data('../data/flight/2007_subset.csv', normalized_data[0], normalized_data[1],
                                  normalized_data[2], normalized_data[3])
 
-    # get the same amount of delayed and on time flights to avoid a bias
+    # reorganize data to avoid a bias
     shuffle(training_data)
-    training_data = filter(training_data)
-    verification_data = filter(verification_data)
+    training_data = order(training_data)
+    verification_data = order(verification_data)
 
-    for layer in range(1, 5, 2):
-        for nodes in range(3, 11, 2):
+    # best parameters
+    best_accuracy = 0.0
+    best_layers = 0
+    best_nodes = 0
+
+    # train and test the network with different parameters
+    for layer in range(1, 4):
+        for nodes in range(3, 11):
             logging.info('Testing with {0} layer(s) and {1} nodes per layer'.format(layer, nodes))
-            weights = None
-            network = train(training_data, 0.3, 10000, layer, nodes, weights)
-            test(network, verification_data)
+            network = train(training_data, 0.3, 10000, layer, nodes)
+            accuracy = test(network, verification_data)
+
+            # remember the best parameters
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_layers = layer
+                best_nodes = nodes
+
+    logging.info(
+        'Best accuracy ({0:.4f}) was achieved with {1} layer(s) and {2} nodes per layer'.format(accuracy, best_layers,
+                                                                                                best_nodes))
 
 
 if __name__ == '__main__':
